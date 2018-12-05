a11ec681647aabd1b95c2968d8c9771a07aa5c1f
Mikołaj Wolicki <wolicki.mikolaj@gmail.com>
2018-12-05 10:08:42 +0100

    Dodaj obsługę serva i ledów

--- parking.py
+++ parking.py
@@ -2,7 +2,34 @@
 # -*- coding: utf-8 -*-
 
 import Adafruit_BBIO.GPIO as GPIO
+import Adafruit_BBIO.PWM as PWM
 import time
+
+servoPin="P9_14"
+PWM.start(servoPin,5,50)
+while(1):
+        dutyCycle=input("What Duty Cycle? ")
+        PWM.set_duty_cycle(servoPin,dutyCycle)
+
+
+#Configuration czujnika odleglosci
+GPIO.setup("P8_12",GPIO.OUT) #Trigger
+GPIO.setup("P8_11",GPIO.IN)  #Echo
+
+#SERVO config
+servoPin="P9_14"
+# PWM.start(servoPin,5,50) TODO Sprawdzic jaki zapis powinien byc
+PWM.start(servoPin, 50)
+
+#led config
+greenLed = "P8_10"
+redLed = "P8_9"
+GPIO.setup("P8_10", GPIO.OUT) #green
+GPIO.setup("P8_9", GPIO.OUT) #red
+
+#Security
+GPIO.output("P8_12", False)
+time.sleep(0.5)
 
 def distanceMeasurement(TRIG,ECHO):
 
@@ -20,13 +47,22 @@ def distanceMeasurement(TRIG,ECHO):
     distance = round(distance, 2)
     return distance
 
-#Configuration
-GPIO.setup("P8_12",GPIO.OUT) #Trigger
-GPIO.setup("P8_11",GPIO.IN)  #Echo
 
-#Security
-GPIO.output("P8_12", False)
-time.sleep(0.5)
+def openServo():
+    desiredAngle = 10
+    dutyCycle = 1. / 18. * desiredAngle + 2
+    PWM.set_duty_cycle(servoPin, dutyCycle)
+
+def closeServo():
+    desiredAngle = 30
+    dutyCycle = 1. / 18. * desiredAngle + 2
+    PWM.set_duty_cycle(servoPin, dutyCycle)
+
+def ledOn (ledPin):
+    GPIO.output(ledPin, GPIO.HIGH)
+
+def ledOff (ledPin):
+    GPIO.output(ledPin, GPIO.LOW)
 
 #main Loop
 try:
