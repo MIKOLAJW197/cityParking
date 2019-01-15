440aa65243db46b7ca411e922881ffe5a8b3aaf1
Mikołaj Wolicki <42266201+MIKOLAJW197@users.noreply.github.com>
2019-01-15 19:26:04 +0100

    Final Commit

--- parking.py
+++ parking.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import _thread
+import thread
 import time
+import requests
 
 import Adafruit_BBIO.GPIO as GPIO
 import Adafruit_BBIO.PWM as PWM
-from flask import Flask, jsonify
-from flask import request
-from flask_cors import CORS, cross_origin
 
 # Configuration czujnika odleglosci
 GPIO.setup("P9_12", GPIO.OUT, initial=GPIO.LOW)  # Trigger
@@ -24,48 +22,33 @@ redLed = "P8_9"
 GPIO.setup("P8_10", GPIO.OUT)  # green
 GPIO.setup("P8_9", GPIO.OUT)  # red
 
-# config of restful-api
-app = Flask(__name__)
-cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
 
 # note: 1 - wolny/odblokowany , 2 - zajety samochod, 3 - zarezerowwany
 parking = [
-    {"id": 0,
-     "type": "1"},
-
     {"id": 1,
-     "type": "1"}
+     "type": 0}
 ]
 
 
-@app.route('/', methods=['GET'])
-@cross_origin()
-def api_root():
-    if request.method == 'GET':
-        return jsonify(parking)
+def checkPlace():
+    while(1):
+        r = requests.get("http://aieozn.pl/sw/isOccupied/1")
+        print(r.text)
+        time.sleep(2)
+        if(r.text == '2' and parking[0]['type'] != 2):
+            print('blokada')
+            closeServo()
+            ledOn(redLed)
+            ledOff(greenLed)
+            parking[0]['type'] = int(r.text)
+        else:
+            if(parking[0]['type'] == 2 and r.text != '2'):
+                openServo()
+                ledOn(greenLed)
+                ledOff(redLed)
+                print('unblock')
+                parking[0]['type'] = int(r.text)
 
-
-@app.route('/block/<id>', methods=['GET'])
-@cross_origin()
-def api_block(id):
-    for spot in parking:
-        if (int(id) == spot['id']):
-            spot['type'] = 3
-    return jsonify(parking)
-
-
-@app.route('/unblock/<id>', methods=['GET'])
-@cross_origin()
-def api_unblock(id):
-    for spot in parking:
-        if (int(id) == spot['id']):
-            spot['type'] = 1
-        return jsonify(parking)
-
-
-def serverStart():
-    app.run()
 
 
 def distanceMeasurement(TRIG, ECHO):
@@ -104,20 +87,49 @@ def ledOff(ledPin):
     GPIO.output(ledPin, GPIO.LOW)
 
 
-def measureDistanceLoop():
-    try:
-        while True:
-            recoveredDistance = distanceMeasurement("P9_12", "P9_15")
-            print("Distance: " + recoveredDistance + "cm")
-            time.sleep(1)
-    except KeyboardInterrupt:
-        print("Measurement stopped by user")
-        GPIO.cleanup()
+def measure_average():
+    d1 = distanceMeasurement("P9_12", "P9_15")
+    d2 = distanceMeasurement("P9_12", "P9_15")
+    d3 = distanceMeasurement("P9_12", "P9_15")
+    d4 = distanceMeasurement("P9_12", "P9_15")
+    distance = (d1 + d2 + d3 + d4) / 4
 
+    return distance
 
 # main Loop
 if __name__ == '__main__':
-    _thread.start_new_thread(serverStart,())
+    # _thread.start_new_thread(serverStart,())
+    ledOn(greenLed)
+    ledOff(redLed)
+    # thread.start_new_thread( checkPlace, ())
     while(1):
-        print()
-
+        r = requests.get("http://aieozn.pl/sw/isOccupied/1")
+        if (r.text != '2'):
+            if (parking[0]['type'] == 2):
+                openServo()
+                ledOn(greenLed)
+                ledOff(redLed)
+                print('unblock')
+                parking[0]['type'] = int(r.text)
+            else:
+                recoveredDistance = measure_average()
+                print('measure distance')
+                if (recoveredDistance < 10):
+                    ledOff(greenLed)
+                    ledOn(redLed)
+                    parking[0]['type'] = 1
+                    r = requests.get("http://aieozn.pl/sw/take/1")
+                    time.sleep(10)
+                else:
+                    ledOff(redLed)
+                    ledOn(greenLed)
+                    parking[0]['type'] = 0
+                    r = requests.get("http://aieozn.pl/sw/leave/1")
+        else:
+            if (parking[0]['type'] != 2):
+                print('blokada')
+                closeServo()
+                ledOn(redLed)
+                ledOff(greenLed)
+                parking[0]['type'] = int(r.text)
+            time.sleep(5)
